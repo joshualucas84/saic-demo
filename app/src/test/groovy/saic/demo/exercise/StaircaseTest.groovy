20eadbe7db3ea1d9c77d96d4ce3e49cc284a82f0
Joshua Lucas <jlucas5190@gmail.com>
2022-12-14 09:50:24 -0600

    updates to gradle

---
+++ app/src/test/groovy/saic/demo/exercise/StaircaseTest.groovy
@@ -0,0 +1,26 @@
+package saic.demo.exercise
+
+import saic.demo.App
+import saic.demo.excercise.Staircase
+import spock.lang.Specification
+
+class StaircaseTest  extends Specification{
+    def "Kid has a bunch of steps"() {
+        given:
+        def  staircase = new Staircase()
+
+        when:
+        def actual = staircase.countWaysDP(steps, testInput)
+
+        then: "I should get the frequency count of the max number in the integer array"
+        actual == "$expectedResult"
+
+        where:
+        testInput                         | steps   |     expectedResult
+        [1..10].dump()  as int []         | 4       |     97
+        [1..20].dump()  as int []         | 1       |     106
+
+
+    }
+
+}
